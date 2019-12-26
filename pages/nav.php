f8b64f2dfc65c4fe01aa5dde549a6f398ec796db
Juanillo52 <juanillo.box@gmail.com>
2019-12-26 12:41:37 +0100

    header y nav semiterminados, plantilla en pensiones.php

---
+++ pages/nav.php
@@ -0,0 +1,18 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="dashboard.php">Dashboard </a>
+                </li>
+                <li class="menu-title">Productos</li><!-- /.menu-title -->
+                <li><a href="accounts.php">Cuentas</a></li>
+                <li><a href="cards.php">Tarjetas</a></li>
+                <li><a href="transfers.php">Transferencias</a></li>
+                <li><a href="loans.php">Préstamos</a></li>
+                <li><a href="pensionplans.php">Plan de pensiones</a></li>
+                <li><a href="new_transfer.php">Nueva transferencia</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>

f890fcd83d40146ba6913cd1ced5cd7e81677980
EichZz <hectoramm.rubio@gmail.com>
2019-12-27 11:54:21 +0100

    comprobración SESSION y varias correcciones

--- pages/loans.php
+++ pages/loans.php
@@ -1,7 +1,9 @@
 <?php
-    session_start();
+session_start();
+    if(!isset($_SESSION['login']) || !$_SESSION['login']){
+        header('Location: login.php');
+    }
 ?>
-
 
 <?php
     function mostrarPrestamos(){

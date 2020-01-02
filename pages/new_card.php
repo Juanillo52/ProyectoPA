8fe615c329bc414544f0debcd0acd51cf3d21d60
EichZz <hectoramm.rubio@gmail.com>
2020-01-02 16:10:09 +0100

    Conexión bd entera

--- pages/new_card.php
+++ pages/new_card.php
@@ -144,7 +144,7 @@
         $cvv = intval($cvv);
         $pin = intval($pin);
 
-        $con = mysqli_connect("68.183.69.142", "root", "");
+        $con = mysqli_connect("68.183.69.142", "root", "Pistacho99!");
         
         if(!$con){
             die('No puedo conectar: ' . mysqli_error($con));
@@ -187,7 +187,7 @@
     function obtenerCuentas(){
         $cliente = $_SESSION['dni'];
         $cuentas = [];
-        $con = mysqli_connect("68.183.69.142","root","");
+        $con = mysqli_connect("68.183.69.142","root","Pistacho99!");
 
         if (!$con){
             die(' No puedo conectar: ' . mysqli_error($con));
--- pages/new_transfer.php
+++ pages/new_transfer.php
@@ -122,7 +122,7 @@
         $fecha = date('Y-m-d');
         
 
-        $con = mysqli_connect("68.183.69.142", "root", "");
+        $con = mysqli_connect("68.183.69.142", "root", "Pistacho99!");
         
         if(!$con){
             die('No puedo conectar: ' . mysqli_error($con));
@@ -270,7 +270,7 @@
     function obtenerCuentas(){
         $cliente = $_SESSION['dni'];
         $cuentas = [];
-        $con = mysqli_connect("68.183.69.142","root","");
+        $con = mysqli_connect("68.183.69.142","root","Pistacho99!");
 
         if (!$con){
             die(' No puedo conectar: ' . mysqli_error($con));
